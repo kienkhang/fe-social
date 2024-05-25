c3c012c19f1176abdcefdd72a148d7f09f84ad7a
kienkhanglab <tkkhang@troodonlabs.com>
2024-05-25 15:43:55 +0700

    feat: install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/route";
+import { createPinia } from "pinia";
 
 let app = createApp(App);
+const pinia = createPinia();
 
 function initCommon() {
+  app.use(pinia);
   app.use(router);
   app.mount("#app");
 }
