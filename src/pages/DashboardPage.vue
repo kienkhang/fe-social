287ca7edd51b143f348a78089b2bbf650a144828
kienkhang <trankienkhang2210@gmail.com>
2024-06-06 01:43:39 +0700

    feat: demo icon

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,8 +1,15 @@
 <template lang="pug">
 div.text-2xl.font-bold Dashboard Page
+  .flex.items-center.gap-3
+    AppIcon.text-red-500.w-10(:icon='RingIcon')
+    AppIcon.text-green-500.h-10.w-10(:icon='GGIcon' mode='bg')
 
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppIcon from "@/components/shared/AppIcon.vue";
+import RingIcon from "@/assets/ring.svg";
+import GGIcon from "@/assets/google.svg";
+</script>
 
 <style scoped></style>
