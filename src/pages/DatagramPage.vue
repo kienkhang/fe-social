826c2336f0413d7a84158e91cee2fc42097fd346
kienkhang <trankienkhang2210@gmail.com>
2024-12-26 18:57:12 +0700

    feat: update

--- src/pages/DatagramPage.vue
+++ src/pages/DatagramPage.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 div
   button.p-2.border.rounded-full.bg-blue-500.text-white(@click='startRoom()') Start room
-  div#datagram-fram.w-full.bg-black(ref='root' class='min-h-[600px]')
+  div#datagram-frame.w-full.bg-black(ref='root' :class='[{ "fixed z-[10] block inset-0" : iframeMounted }]' v-show='iframeMounted')
 
 </template>
 
@@ -15,6 +15,7 @@ import {
 import { onMounted, ref } from "vue";
 
 const root = ref<HTMLElement>();
+const iframeMounted = ref(false);
 const isLoading = ref(false);
 
 let client: Client;
@@ -22,7 +23,7 @@ let conference: Conference;
 
 const options: IConferenceOptions = {
   skipMediaSettings: false,
-  turnOnCam: true,
+  turnOnCam: false,
   turnOnMic: false,
   authorization:
     "SFMyNTY.YWIxNGM5MDktODJkMS00YThjLWJhMDItNWJkYTcxN2I4NmFj.D8BDzpMBlkkMwKP7JQvI1ieSo5pSqoWRw9qf2cYUe3A",
@@ -30,9 +31,11 @@ const options: IConferenceOptions = {
 
 useEventListener("message", async (event) => {
   if (event.data === "call_ended") {
+    iframeMounted.value = false;
     conference?.dispose();
   }
   if (event.data === "invalid_qr_code") {
+    iframeMounted.value = false;
     conference?.dispose();
   }
 });
@@ -40,13 +43,15 @@ useEventListener("message", async (event) => {
 async function startRoom() {
   // Create client with alias and origin
   client = Client.create({
-    alias: "bkaui1",
+    alias: "bkaui",
     origin: "http://localhost:8080",
   });
 
   isLoading.value = true;
   conference = new Conference(client, options);
   await conference.mount(root.value as HTMLElement).finally(() => {
+    console.log("Mounted room");
+    iframeMounted.value = true;
     isLoading.value = false;
   });
 }
@@ -54,10 +59,6 @@ async function startRoom() {
 
 <style lang="stylus" scoped>
 #datagram-frame
-  display block !important
-  z-index 10
-  position fixed
-  inset 0
   height 100vh
   @media mobile
     height 100dvh
